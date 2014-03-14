ff8ff764fecd5bc7eed42bd3aa31671993a8133c
Ryan Punt <Ryan.Punt@bluestembrands.com>
2014-03-14 15:31:13 -0500

    removed vars from packages recipe

---
+++ tmp/foodcritic/cookbook/attributes/default.rb
@@ -0,0 +1,13 @@
+#default["collectd"]["version"]            = "5.4.0"
+default["collectd"]["dir"]                = "/etc/collectd"
+default["collectd"]["confdir"]            = "/etc/collectd/plugins.d"
+default["collectd"]["plugindir"]          = "/usr/lib/collectd"
+default["collectd"]["typesdbdir"]         = "/usr/share/collectd"
+#default["collectd"]["url"]                = "http://collectd.org/files/collectd-#{node["collectd"]["version"]}.tar.gz"
+#default["collectd"]["checksum"]           = "cb361deeda00bece54cec2f7a2c368ccea4ac1c4a83388a0dae0435ba8969ee1"
+default["collectd"]["interval"]           = 10
+default["collectd"]["read_threads"]       = 5
+default["collectd"]["name"]               = node["fqdn"]
+default["collectd"]["plugins"]            = Mash.new
+default["collectd"]["graphite_role"]      = "graphite"
+default["collectd"]["graphite_ipaddress"] = ""
