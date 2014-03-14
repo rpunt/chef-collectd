89f1171b8cc671d326f0e0c3799511466f4fd7c5
Ryan Punt <Ryan.Punt@bluestembrands.com>
2014-03-14 15:12:29 -0500

    updated conf template for package-based install

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,8 +1,10 @@
 default["collectd"]["version"]            = "5.4.0"
-default["collectd"]["dir"]                = "/opt/collectd"
-default["collectd"]["confdir"]            = "#{node["collectd"]["dir"]}/etc/conf.d"
-default["collectd"]["url"]                = "http://collectd.org/files/collectd-#{node["collectd"]["version"]}.tar.gz"
-default["collectd"]["checksum"]           = "cb361deeda00bece54cec2f7a2c368ccea4ac1c4a83388a0dae0435ba8969ee1"
+default["collectd"]["dir"]                = "/etc/collectd"
+default["collectd"]["confdir"]            = "#{node["collectd"]["dir"]}/plugins.d"
+default["collectd"]["plugindir"]          = "#{node["collectd"]["dir"]}/plugins.d"
+default["collectd"]["typesdbdir"]         = "/usr/share/collectd"
+#default["collectd"]["url"]                = "http://collectd.org/files/collectd-#{node["collectd"]["version"]}.tar.gz"
+#default["collectd"]["checksum"]           = "cb361deeda00bece54cec2f7a2c368ccea4ac1c4a83388a0dae0435ba8969ee1"
 default["collectd"]["interval"]           = 10
 default["collectd"]["read_threads"]       = 5
 default["collectd"]["name"]               = node["fqdn"]
